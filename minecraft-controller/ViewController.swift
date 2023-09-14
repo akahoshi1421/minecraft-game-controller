a1c370683301917d27512f603a96c54ada81341c
akahoshi1421 <akaho1421@gmail.com>
2023-09-14 19:21:07 +0900

    Initial Commit

---
+++ minecraft-controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  minecraft-controller
+//
+//  Created by 赤星宏樹 on 2023/09/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
